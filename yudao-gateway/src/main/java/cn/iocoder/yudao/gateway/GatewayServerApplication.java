a6788258efe7eb7e2603eb6907b3705b4bbf564b
hicode <hicode@163.com>
2026-03-24 08:42:27 +0800

    测试git配置

--- yudao-gateway/src/main/java/cn/iocoder/yudao/gateway/GatewayServerApplication.java
+++ yudao-gateway/src/main/java/cn/iocoder/yudao/gateway/GatewayServerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GatewayServerApplication {
 
     public static void main(String[] args) {
+
         // 启动 Spring Boot 应用
         SpringApplication.run(GatewayServerApplication.class, args);
     }
